586f31d5136502e33fa5d3f261b7876874521570
Adewale Olaleye <adewale@Adewales-MacBook-Pro.local>
2022-09-06 18:15:50 -0500

    update

--- sonarqube.sh
+++ sonarqube.sh
@@ -7,47 +7,52 @@
 
 echo “Installation of SonarQube in progress. Please be patient…”
 
-#This process need to be run as regular user (Sonarqube doesn’t work when you start it as root user). #The only prerequisite for running SonarQube is to have Java (Oracle JRE 11 or OpenJDK 11) installed on your machine. To install OpenJDK 11, run the following commands
+#This process need to be run as regular user (Sonarqube doesn’t work when you start it as root user).
+
 echo “Install java 11:”
-$ sudo yum update -y
-$ sudo yum install java-11-openjdk-devel -y 
-$ sudo yum install java-11-openjdk -y
+sudo yum update -y
+sudo yum install java-11-openjdk-devel -y 
+sudo yum install java-11-openjdk -y
 echo
 
 echo “Navigate to the /opt directory:”
-$ cd /opt 
+cd /opt 
 echo
 
 echo “Install wget:”
-$ sudo yum install wget -y
+sudo yum install wget -y
 echo
 
 echo “Download and install SonarQube package:”
-$ sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.3.0.51899.zip
+sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.3.0.51899.zip
 echo
 
 echo “Install unzip:”
-$ sudo yum install unzip -y
+sudo yum install unzip -y
 echo
 
 echo “Unzip the previously installed package:”
-$ sudo unzip /opt/sonarqube-9.3.0.51899.zip
+sudo unzip /opt/sonarqube-9.3.0.51899.zip
 echo
 
 echo “Change ownership to the user:”
-$ sudo chown -R vagrant:vagrant /opt/sonarqube-9.3.0.51899
+sudo chown -R vagrant:vagrant /opt/sonarqube-9.3.0.51899
 echo
 
 echo “Switch to linux binaries directory:”
-$ cd /opt/sonarqube-9.3.0.51899/bin/linux-x86-64
+cd /opt/sonarqube-9.3.0.51899/bin/linux-x86-64
 echo
 
 echo “Start the daemons:”
-$ ./sonar.sh start
+./sonar.sh start
 echo
 
 echo “Check Sonarqube status:”
-$ ./sonar.sh status
+./sonar.sh status
+echo
+
+echo "Get IP address:"
+hostname -I
 echo
 
 #Connect to the Sonarqube server through the browser by using the url:
